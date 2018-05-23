6cfa9adb554cb03b135cd064774f8fb658981ecb
donjoe <giumontanaro@gmail.com>
2018-05-23 15:29:13 +0200

    make the name, date and rating of reviews bolded

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -120,14 +120,17 @@ createReviewHTML = (review) => {
   const li = document.createElement('li');
   const name = document.createElement('p');
   name.innerHTML = review.name;
+  name.classList.add("reviews-details");
   li.appendChild(name);
 
   const date = document.createElement('p');
   date.innerHTML = review.date;
+  date.classList.add("reviews-details");
   li.appendChild(date);
 
   const rating = document.createElement('p');
   rating.innerHTML = `Rating: ${review.rating}`;
+  rating.classList.add("reviews-details");
   li.appendChild(rating);
 
   const comments = document.createElement('p');
